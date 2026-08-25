4006d05aa89beab92b5f987d968508401d2595ae
Abdulslam AlHadidi <slmhadidi@gmail.com>
2026-08-25 13:59:58 +0300

    Add MultiplyFunction and update Main to use it

--- Program.cs
+++ Program.cs
@@ -8,12 +8,20 @@ namespace Module_6_______Merging_Branches_Using_Visual_Studio
 {
     internal class Program
     {
+
+
+        static int MultiplyFunction(int num1,int num2)
+        {
+            return num1 * num2;
+        }
+
         static void Main(string[] args)
         {
 
             Console.WriteLine("Hello");
             Console.WriteLine("New Branch Created");
 
+            Console.WriteLine(MultiplyFunction(1,66).ToString());
         }
     }
 }
